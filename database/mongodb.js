681bf2281f9bae4727eca4462f231354717797e5
Manolinhopng <145302101+Manolinhopng@users.noreply.github.com>
2024-11-07 15:49:26 -0500

    Update mongodb.js

--- database/mongodb.js
+++ database/mongodb.js
@@ -2,6 +2,7 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 
+// Crea una instancia persistente de MongoClient para mantener la conexión abierta
 const client = new MongoClient(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -9,8 +10,14 @@ const client = new MongoClient(process.env.MONGO_URI, {
 
 async function connectToDatabase() {
   try {
+    // Conexión a la base de datos
     await client.connect();
     console.log("Conexión general a MongoDB establecida.");
+
+    // Asegúrate de que el cliente se mantenga conectado mientras la aplicación esté en ejecución
+    client.on("topologyClosed", () => {
+      console.log("La topología de MongoDB se ha cerrado.");
+    });
   } catch (error) {
     console.error("Error al conectar a MongoDB:", error);
   }
